f3a0ccf9add189d9603270a15230cf3f333eab36
João Pedro <jpedro_15@outlook.com>
2022-08-28 13:13:37 -0300

    att

--- Lista 3/L3e1/main.c
+++ Lista 3/L3e1/main.c
@@ -25,15 +25,70 @@ void imprimir_pessoa(Pessoa p){
     printf("\nNome: %s\nData: %2d/%2d/%4d\n", p.nome, p.data.dia, p.data.mes, p.data.ano);
 }
 
+//push -> empilha
+No* push(No *topo){
+    No *novo = malloc(sizeof(No));
+    if(novo){
+        novo-> p = ler_pessoa();
+        novo-> proximo = topo;
+        return novo;
+    }
+    else
+        printf("\nErro ao alocar memoria!!!\n");
+    return NULL;
+}
+
+//pop -> desempilha
+No* pop(No **topo){
+    if(*topo != NULL){
+        No *remover = *topo;
+        *topo = remover->proximo;
+        return remover;
+    }
+    else
+        printf("\nPilha vazia!\n");
+    return NULL;
+}
+
+//imprimir pilha
+void imprimir_pilha(No *topo){
+    printf("\n------------ Pilha -----------\n");
+    while(topo){
+        imprimir_pessoa(topo->p);
+        topo = topo->proximo;
+    }
+    printf("\n---------- Fim Pilha ----------\n");
+}
 
 int main()
 {
-    No *topo=NULL;
+    No *remover, *topo=NULL;
     int opcao;
 
     do{
-        //printf()
-    }while(opcao!=1);
+        printf("\n0 - Sair\n1 - empilhar\n2 - desempilhar\n3 - imprimir\n");
+        scanf("%d",&opcao);
+        getchar();
+        switch(opcao){
+            case 1:
+                topo=push(topo);
+                break;
+            case 2:
+                remover=pop(&topo);
+                if(remover){
+                    printf("\n\nElemento removido com sucesso!\n");
+                    imprimir_pessoa(remover->p);
+                }else
+                    printf("\nSem no a remover.\n");
+                break;
+            case 3:
+                imprimir_pilha(topo);
+                break;
+            default:
+                if(opcao!=0)
+                    printf("\nOpcao invalida!\n");
+        }
+    }while(opcao!=0);
 
     return 0;
 }
