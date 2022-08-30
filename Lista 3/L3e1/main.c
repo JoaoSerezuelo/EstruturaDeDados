e050738d8adebbad0c0af373be98ccaed7c54710
João Pedro <jpedro_15@outlook.com>
2022-08-30 16:24:06 -0300

    att

--- Lista 3/L3e1/main.c
+++ Lista 3/L3e1/main.c
@@ -15,7 +15,6 @@ typedef struct no{
     Pessoa p;
     struct no *proximo;
 }No;
-
 //struct pilha
 typedef struct {
     No *topo;
@@ -24,11 +23,12 @@ typedef struct {
 
 
 
-//Fun��es Pessoa
+//Funções Pessoa
 Pessoa ler_pessoa(){
     Pessoa p;
     printf("\nDigite nome e data de nascimento dd mm aaaa:\n");
     scanf("%49[^\n]%d%d%d",p.nome,&p.data.dia,&p.data.mes,&p.data.ano);
+    fflush(stdin);
     return p;
 }
 void imprimir_pessoa(Pessoa p){
@@ -36,11 +36,88 @@ void imprimir_pessoa(Pessoa p){
 }
 
 //Pilha
-
 //criando pilha
 void criar_pilha(Pilha *p){
     p->topo=NULL;
     p->tam =0;
 }
+//push -> empilhar
+void push(Pilha *p){
+    No *novo = malloc(sizeof(No));
+    if(novo){
+        novo-> p = ler_pessoa();//ler pessoa
+        novo-> proximo = p->topo;//clona o topo antigo para o proximo da nova stutura que ta no topo
+        p->topo = novo;//topo é atualizado para o endereço da nova estrutura que esta no topo
+        p->tam++;//tamanho da pilha é atualizado
+    }
+    No *novo2=malloc(sizeof(No));
+    if(novo2){
+        novo2-> p = ler_pessoa();//ler pessoa
+        novo2-> proximo = p->topo;//clona o topo antigo para o proximo da nova stutura que ta no topo
+        p->topo = novo2;//topo é atualizado para o endereço da nova estrutura que esta no topo
+        p->tam++;//tamanho da pilha é atualizado
+    }
+    else
+        printf("\nErro ao alocar memoria!!!\n");
+}
+//pop -> desempilhar
+No* pop(Pilha *p){
+    if(p->topo){
+        No *remover = p->topo;//remover recebe o topo da pilha
+        p->topo = remover->proximo;//topo da pilha recebe o proximo da pilha(posição da estrutura que está embaixo)
+        p->tam--;//tamanho da pilha é atualizado
+        return remover;//retorna a estrutura que foi removida
+    }
+    else
+        printf("\nPilha vazia!\n");
+    return NULL;
+}
+//imprimir pilha
+void imprimir_pilha(Pilha *p){
+    No *aux = p->topo;//aux recebe o topo da pilha
+    printf("\n------------ Pilha tam: %i-----------\n",p->tam);
+    while(aux){//enquanto aux for diferente de NULL
+        imprimir_pessoa(aux->p);//imprime a pessoa que esta sendo apontada por aux
+        aux = aux->proximo;//aux vai descendo na pilha
+    }
+    printf("\n---------- Fim Pilha ----------\n");
+}
+int main()
+{
+    No *remover;//remover para recebe o topo da pilha
+    Pilha p;//pilha
+    int opcao;
 
+    criar_pilha(&p);
+
+    do{
+        printf("\n0 - Sair\n1 - empilhar\n2 - desempilhar\n3 - imprimir\n");
+        scanf("%d",&opcao);
+        fflush(stdin);
+        getchar();
+        switch(opcao){
+            case 1:
+                push(&p);
+                break;
+            case 2:
+                remover=pop(&p);
+                if(remover){
+                    printf("\n\nElemento removido com sucesso!\n");
+                    imprimir_pessoa(remover->p);//imprime a pessoa que foi removida
+                    free(remover);//livra a memoria alocada para a pessoa que foi removida
+                    remover=NULL;
+                }else
+                    printf("\nSem no a remover.\n");
+                break;
+            case 3:
+                imprimir_pilha(&p);
+                break;
+            default:
+                if(opcao!=0)
+                    printf("\nOpcao invalida!\n");
+        }
+    }while(opcao!=0);
+
+    return 0;
+}
 
