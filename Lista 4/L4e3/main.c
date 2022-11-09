327eec5a924ff60f1afbfa0c26b5118a68bff544
João Pedro <jpedro_15@outlook.com>
2022-11-09 17:22:21 -0300

    att

--- Lista 4/L4e3/main.c
+++ Lista 4/L4e3/main.c
@@ -23,6 +23,22 @@ void criar_pilha(Pilha *p){
     p->topo=NULL;
     p->tam=0;
 }
+
+Pilha *criarPilha() {
+    Pilha *pilha = (Pilha*)malloc(sizeof(Pilha));
+    if(!pilha) {
+        printf("Erro de alocacao de memoria");
+        return NULL;
+    }
+
+    return pilha;
+}
+
+void inicializar(Pilha *pilha) {
+    pilha->topo = NULL;
+    pilha->tam = 0;
+}
+
 void push(Pilha *p,int num){
     No *novo=malloc(sizeof(No));
     if(novo){
@@ -55,14 +71,14 @@ void inserir_na_fila(Fila *fila, int num){
     No *novo=malloc(sizeof(No));
     if(novo){
         novo->valor=num;
-        novo->proximo=NULL;//TÁ INSERINDO NO FINAL POR ISSO PROXIMO VAI SER SEMPRE NULO
+        novo->proximo=NULL;//TÃ INSERINDO NO FINAL POR ISSO PROXIMO VAI SER SEMPRE NULO
         if(fila->primeiro==NULL){//FILA TA VAZIA AINDA
-            //posições sendo preenchidas com o no novo
+            //posiÃ§Ãµes sendo preenchidas com o no novo
             fila->primeiro=novo;
             fila->fim=novo;
         }
-        else{//fila não tá vazia
-            fila->fim->proximo=novo;//a variavel que tava no fim agora aponta p/ nova variavel que tá no fim
+        else{//fila nÃ£o tÃ¡ vazia
+            fila->fim->proximo=novo;//a variavel que tava no fim agora aponta p/ nova variavel que tÃ¡ no fim
             fila->fim=novo;//fim da fila recebe o novo no;
         }
         fila->tam++;
@@ -75,14 +91,14 @@ No* remover_da_fila(Fila *fila){
 
     if(fila->primeiro){
         remover=fila->primeiro;//removi o primeiro elmento da fila
-        fila->primeiro=remover->proximo;//primeiro elemneto da fila é atualizado
+        fila->primeiro=remover->proximo;//primeiro elemneto da fila Ã© atualizado
         fila->tam--;
     }
     else
         printf("\n\nFila vazia\n");
     return remover;
 }
-void imprimir_fila(Fila fila){
+void imprimir_fila(Fila *fila){
     No *aux=NULL;
     aux=fila->primeiro;
     printf("\n----- FILA ---------\n");
@@ -93,29 +109,46 @@ void imprimir_fila(Fila fila){
     printf("\n----- FIM FILA -----\n");
 }
 
-//iverte fila usando pilha
-void inverte(Fila *fila){
-    Pilha p;
-    No *removido;
-    int aux;
-    criar_pilha(&p);
-    //esvazio a fila e preencho a pila
-    while(fila){
-        removido=remover_da_fila(&fila);
-        aux=removido->valor;
-        free(removido);
-        push(&p,aux);
+void liberarPilha(Pilha *pilha) {
+    No *no = pilha->topo;
+    if(!no) {
+        printf("Pilha vazia");
+        return;
     }
-    //esvazio a pilha e preencho a fila
-    while(p){
-        removido=pop(&p);
-        aux=removido->valor;
-        free(removido);
-        inserir_na_fila(&fila,aux);
+
+    while(no->proximo) {
+        pop(pilha);
+        no = no->proximo;
     }
+
+    free(pilha);
 }
-int main()
-{
+
+//iverte fila usando pilha
+void inverte(Fila *fila) {
+
+    Pilha *pilha = criarPilha();
+    inicializar(pilha);
+
+    No *no = fila->primeiro;
+
+    while(no) {
+        push(pilha, no->valor);
+        no = no->proximo;
+        remover_da_fila(fila);
+    }
+
+    no = pilha->topo;
+
+    while(no) {
+        inserir_na_fila(fila, no->valor);
+        no = no->proximo;
+    }
+
+    liberarPilha(pilha);
+}
+
+int main() {
     No *removido;
     Fila fila;
     int op,valor;
