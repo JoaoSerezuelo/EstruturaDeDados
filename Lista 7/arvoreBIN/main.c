6a17d46abb87f48b60153e92775bd5235ee4878d
João Pedro <jpedro_15@outlook.com>
2022-11-21 15:19:22 -0300

    att

--- Lista 7/arvoreBIN/main.c
+++ Lista 7/arvoreBIN/main.c
@@ -6,27 +6,46 @@ typedef struct no{
     struct no *direita, *esquerda;
 }NoArv;
 
- NoArv* inserir_versao1(NoArv *raiz, int num){
-    if(raiz==NULL){
-        NoArv *aux=malloc(sizeof(NoArv));//como so vou trabalhar com arvores pequenas nao vou verificar se foi alocado
-        aux->valor=num;
-        aux->direita=NULL;
-        aux->esquerda=NULL;
-        return aux;
-    }else{
-        if(num<raiz->valor){
-            raiz->esquerda=inserir_versao1(raiz->esquerda,num);
+ void inserir_versao2(NoArv **raiz, int num){
+    if(*raiz==NULL){//primeiro no
+        *raiz=malloc(sizeof(NoArv));
+        (*raiz)->valor=num;
+        (*raiz)->direita=NULL;
+        (*raiz)->esquerda=NULL;
+    }
+    else{
+        if(num<(*raiz)->valor){
+            inserir_versao2(&((*raiz)->esquerda),num);
         }
-        else{
-            raiz->direita=inserir_versao1(raiz->direita,num);
-        }
-        return raiz;
+        else
+            inserir_versao2(&((*raiz)->direita),num);
     }
  }
+//tipos impressao
+/*exemplo arvore:
+        50
+    25      100
+       30
+*/
+void PreOrdem(NoArv *raiz){//Pre Ordenado ex:50 25 30 100
+    if(raiz){
+        printf("%d | ",raiz->valor);
+        PreOrdem(raiz->esquerda);
+        PreOrdem(raiz->direita);
+    }
+}
 
+void EmOrdem(NoArv *raiz){//Em ordem ex:25  30 50 100
+    if(raiz){
+        EmOrdem(raiz->esquerda);
+        printf("%d | ",raiz->valor);
+        EmOrdem(raiz->direita);
+    }
+}
 int main()
 {
     NoArv *raiz=NULL;
-
+    int valor=10;
+    inserir_versao2(&raiz,valor);
     return 0;
 }
